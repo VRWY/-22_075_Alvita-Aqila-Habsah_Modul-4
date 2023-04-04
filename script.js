04c127fe30b6b2d9b4c255337e81fe2991c99f85
Alvita Aqila Habsah <113221647+VRWY@users.noreply.github.com>
2023-04-04 21:28:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Beralih antara menambah dan menghapus kelas "responsif" ke topnav saat pengguna mengklik ikon
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+        }
+    }
+)
